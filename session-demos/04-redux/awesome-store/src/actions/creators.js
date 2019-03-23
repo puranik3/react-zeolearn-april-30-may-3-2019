0c996bbc27392389b69bd20ad350de1ef0c15480
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2019-03-22 23:53:20 -0400

    actions for product detail

--- session-demos/04-redux/awesome-store/src/actions/creators.js
+++ session-demos/04-redux/awesome-store/src/actions/creators.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { LOADING, FETCHING_PRODUCTS, FETCHED_PRODUCTS, FETCH_PRODUCTS_ERRORED } from './constants';
+import { FETCHING_PRODUCT, FETCHED_PRODUCT, FETCH_PRODUCT_ERRORED } from './constants';
 
 export const loadingAC = () => ({
     type: LOADING
@@ -29,6 +30,42 @@ export const fetchingProductsThunk = () => {
 
         axios.get( 'https://awesome-store-server.herokuapp.com/products' )
             .then( response => response.data ) 
+            .then( items => {
+                dispatch( fetchedProductsAC( items ) );
+            })
+            .catch( error => {
+                dispatch( fetchProductsErroredAC( error ) );
+            });
+    };
+};
+
+export const fetchingProductAC = productId => ({
+  type: FETCHING_PRODUCT,
+  payload: {
+    productId: productId
+  }
+})
+
+export const fetchedProductAC = productId => ({
+    type: FETCHED_PRODUCT,
+    payload: {
+        productId: productId
+    }
+});
+
+export const fetchProductErroredAC = error => ({
+    type: FETCH_PRODUCT_ERRORED,
+    payload: {
+        error: error
+    }
+});
+
+export const fetchingProductThunk = () => {
+    return dispatch => {
+        dispatch( fetchingProductAC() );
+
+        axios.get( 'https://awesome-store-server.herokuapp.com/products' )
+            .then( response => response.data )
             .then( items => {
                 dispatch( fetchedProductsAC( items ) );
             })
--- session-demos/04-redux/awesome-store/src/components/ProductDetail.js
+++ session-demos/04-redux/awesome-store/src/components/ProductDetail.js
@@ -3,33 +3,35 @@ import StarRating from './StarRating';
 import { Link } from 'react-router-dom';
 
 class ProductDetail extends Component {
-    render() {
-        const productId = this.props.match.params.id;
-        const product = this.props.location.state;
+  render() {
+    const productId = this.props.match.params.id;
+    const product = this.props.location.state;
 
-        return (
-            <div className="container">
-                <h1>{product.name}</h1>
-                <hr />
-                <div className="row">
-                  <div className="col-sm-4">
-                    <img alt={product.name} src={product.imageUrl} className="img-fluid rounded float-left" />
-                  </div>
-                  <div className="col-sm-8">
-                    <div style={{fontSize: '1.6em'}}><StarRating rating={product.starRating} total={5} /></div>
-                    <p>Product ID: {productId}</p>
-                    <p>Name: {product.name}</p>
-                    <p>Description: {product.description}</p>
-                    <p>Price: ${product.price}</p>
-                    <p>Release Date: {product.releaseDate}</p>
-                  </div>
-                </div>
+    return (
+      <div className="container">
+        <h1>{product.name}</h1>
+        <hr />
+        <div className="row">
+          <div className="col-sm-4">
+            <img alt={product.name} src={product.imageUrl} className="img-fluid rounded float-left" />
+          </div>
+          <div className="col-sm-8">
+            <div style={{fontSize: '1.6em'}}><StarRating rating={product.starRating} total={5} /></div>
+            <p>Product ID: {productId}</p>
+            <p>Name: {product.name}</p>
+            <p>Description: {product.description}</p>
+            <p>Price: ${product.price}</p>
+            <p>Release Date: {product.releaseDate}</p>
+          </div>
+        </div>
 
-                <Link to={this.props.match.url}>Reviews</Link> 
-                <Link to={this.props.match.url + '/reviews/submit'}>Submit a Review</Link> 
-            </div>
-        );
-    }
+        <ul class='nav nav-pills'>
+          <li role='presentation'><Link to={this.props.match.url}>Reviews</Link></li>
+          <li role='presentation'><Link to={this.props.match.url + '/reviews/submit'}>Submit a Review</Link></li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default ProductDetail;
