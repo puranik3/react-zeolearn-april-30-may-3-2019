86ecfe0675af5528e94c930cd72ef1c3c568c797
Prashanth Puranik <puranik@digdeeper.in>
2019-04-30 16:11:30 -0400

    intro to reacct

---
+++ puranik3-demos/03-react/github-page.jsx
@@ -0,0 +1,7 @@
+<GitHubPage>
+    <NavBar>
+        <SearchBar />
+        <NavBarItems item={[ 'Pull requests', 'Issues' ]} />
+    </NavBar>
+    <StyledButton icon="eye" label={1}>Unwatch</StyledButton>
+</GitHubPage>
