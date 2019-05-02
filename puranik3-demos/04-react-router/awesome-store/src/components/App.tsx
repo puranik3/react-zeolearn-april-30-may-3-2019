88733e5f838393841ecc9b07c656cff5499c3a5e
Prashanth Puranik <puranik@digdeeper.in>
2019-05-02 09:36:47 -0400

    added link for app.tsx

--- puranik3-demos/04-react-router/awesome-store/src/components/App.tsx
+++ puranik3-demos/04-react-router/awesome-store/src/components/App.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import About from './About';
 import ProductList from './ProductList';
+import { Link } from 'react-router-dom';
 
 export default function App( props : {} ) {
     return (
         <React.Fragment>
+            <nav className="navbar navbar-expand navbar-light bg-light">
+                <div className="nav navbar-nav">
+                    <Link className="nav-item nav-link" to="/">Awesome Store</Link>
+                    <Link className="nav-item nav-link" to="/catalog">Catalog</Link>
+                </div>
+            </nav>
             <Switch>
                 <Route from="/" exact={true} component={About} />
                 <Route from="/catalog" component={ProductList} />
